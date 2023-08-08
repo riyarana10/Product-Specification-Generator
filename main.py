d72b444ba7043f15d7935b1b06eb963b9afb418e
Riya Rana <riyarana7865@gmail.com>
2023-08-08 18:08:58 +0530

    added filter functionality to product specification generator

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ class ProductSpecificationGenerator:
         response = llm.predict(prompt)
         return response
     
+    def get_filtered_specifications(self, filteredkeywords, productname, description):
+        prompt = f"""
+        generate specifications of the {productname} according to a list of keywords : {filteredkeywords} \n
+        and fill the keywords with value using product description: {description}
+        return the response in the form of dictionary format. Only use the keywords mentioned in filteredkeywords, avoid adding extra keywords and values.
+        """
+        
+        response = llm.predict(prompt)
+        return response
 
 class InputHnadler:
     
@@ -32,6 +41,15 @@ class InputHnadler:
         description = input("eneter product description:\n")
         return description 
     
+    def get_filter_keywords(filterkeywords):
+        numberofkeywords = int(input("enter number of keywords to filer product specification: "))
+        filterkeywords =  []
+        print("enter keywords: \n")
+        for i in range(0,numberofkeywords):
+            keyword = input()
+            filterkeywords.append(keyword)
+        
+        return filterkeywords
 
 
 
@@ -46,27 +64,32 @@ if __name__ == "__main__":
     print(productskeywords)
         
     productdescription = inputobj.get_description()
-    print(productdescription)
+    # print(productdescription)
     
-    productspecifications = productobj.get_specifications(productskeywords,productname,productdescription)
-    print(productspecifications)
+    # productspecifications = productobj.get_specifications(productskeywords,productname,productdescription)
+    # print(productspecifications)
+    
+    filteredkeywords = inputobj.get_filter_keywords()
+    
+    filteredproductspecification = productobj.get_filtered_specifications(filteredkeywords,productname,productdescription)
+    print(filteredproductspecification)
     
     
     # parse the output to dictionary format
-    cleaned_response = productspecifications.strip().replace('\"', '').replace('    ','').split('\n')
-    specs_dict = {}
+    # cleaned_response = productspecifications.strip().replace('\"', '').replace('    ','').split('\n')
+    # specs_dict = {}
     
-    for line in cleaned_response:
-        parts = line.split(': ')
-        if len(parts) == 2:
-            specs_dict[parts[0]] = parts[1]
+    # for line in cleaned_response:
+    #     parts = line.split(': ')
+    #     if len(parts) == 2:
+    #         specs_dict[parts[0]] = parts[1]
             
-    # exporting data to json file
-    output_filename = f"{productname}_specifications.json"
-    with open(output_filename, 'w') as json_file:
-        json.dump(specs_dict, json_file, indent=4)
+    # # exporting data to json file
+    # output_filename = f"{productname}_specifications.json"
+    # with open(output_filename, 'w') as json_file:
+    #     json.dump(specs_dict, json_file, indent=4)
 
-    print(f"Specifications saved to {output_filename}")
+    # print(f"Specifications saved to {output_filename}")
     
     
     
